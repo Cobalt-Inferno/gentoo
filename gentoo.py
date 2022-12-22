88acc74afca66ab5d8513a849e4749c382db2d84
Your Name <you@example.com>
2022-12-21 21:30:48 -0500

    Update

--- gentoo.py
+++ gentoo.py
@@ -1,5 +1,28 @@
 import json
 import subprocess
+def run_cmd(cmd):
+    subprocess.call(cmd)
+def write_file(file, str):
+    with open(file, 'w') as f:
+        f.write(str)
+    f.close()
+def append_file(file, str):
+    with open(file, 'a') as f:
+        f.write('\n')
+        f.write(str)
+    f.close()
+
+def eselect(opt: str):
+    run_cmd(['eselect', opt, 'list'])
+    ropt = input('Choose option: ')
+    run_cmd(['eselect', opt, 'set', ropt])
+
+def concat_str(strs):
+    tmp = ''
+    for str in strs:
+        tmp += str
+    return tmp
+
 class Drive:
     def __init__(self, drive):
         self.drive = drive
@@ -9,7 +32,9 @@ class Drive:
             subprocess.call(['mount', self.drive['boot'], '/boot'])
     def mount_drive(self, partition, location):
         subprocess.call(['mount', partition, location])
-
+    def set_fstab(self):
+        if 'fs' in self.drive:
+            write_file('/etc/fstab', concat_str([self.drive['boot'] , '  ' , '/boot' , '   ' , self.drive['fs'] , '    ' , 'defaults' , '    ' , '0 2']))
 class Users:
     def __init__(self,users):
         self.users = users
@@ -49,28 +74,55 @@ class Config:
         def init_user():
             if 'users' in self.config:
                 return Users(self.config['users'])
+        def portage():
+            if 'portage' in self.config:
+                return Portage(self.config['portage'])
         return dict([
             ('users', init_user()),
-            ('drive', init_drive())
+            ('drive', init_drive()),
+            ('portage', portage())
             ])
 
-def run_cmd(cmd):
-    subprocess.call(cmd)
-
-def eselect(opt: str):
-    run_cmd(['eselect', opt, 'list'])
-    ropt = input('Choose option: ')
-    run_cmd(['eselect', opt, 'set', ropt])
-
-class Emerge:
-    def __init__(self) -> None:
-        self.init()
+    def generic(self):
+        if 'timezone' in self.config:
+            write_file('/etc/timezone', self.config['timezone'])
+        if 'init-system' in self.config:
+            if self.config['init-system'] == 'openrc':
+                run_cmd(['emerge', '--config', 'sys-libs/timezone-data'])
+            elif self.config['init-system'] == 'systemd':
+                run_cmd(['ln -sf ../usr/share/zoneinfo/' ++ self.config['timezone'], '/etc/localtime'])
+        if 'locale' in self.config:
+            write_file('/etc/locale.gen', self.config['locale'])
+            run_cmd(['locale-gen'])
+            eselect('locale')           
+            run_cmd(['env-update'])
+        run_cmd(['emerge', '--ask', 'sys-kernel/linux-firmware'])
+class Portage:
+    def __init__(self, config) -> None:
+        self.config = config
+        pass
     def init(self):
         run_cmd(['emerge-webrsync'])
         run_cmd(['emerge', '--sync', '--quiet'])
         eselect('profile')
     def world(self):
+        self.init()
         run_cmd(['emerge', '--ask', '--verbose', '--update', '--deep', '--newuse', '@world'])
+    def install_pkg(self, pkg):
+        run_cmd(['emerge', '--ask', pkg])
+    def set_use_flags(self):
+        with open('/etc/portage/make.conf', 'a') as f:
+            if 'use-flags' in self.config:
+                f.write('\n')
+                f.write('USE="{}"'.format(' '.join(self.config['use-flags'])))
+                f.write('\n')
+                f.write('ACCEPT_LICENSE="-* @FREE @BINARY-REDISTRIBUTABLE"')
+        f.close()
+    def install_kernel(self):
+        eselect('kernel')
+        self.install_pkg('sys-kernel/genkernel')
+    def genkernel(self):
+        run_cmd(['genkernel', 'all'])
 def load_config() -> Config:
     with open('config.json') as f:
         return Config(json.load(f))
@@ -78,5 +130,9 @@ def load_config() -> Config:
 x = load_config()
 v = x.conf['drive']
 v.mount_boot_drive()
-b = Emerge()
-b.world()
+emerge = x.config['portage']
+emerge.set_use_flags()
+x.generic()
+emerge.install_kernel()
+v.set_fstab()
+emerge.genkernel()
